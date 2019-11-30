22cbaddfbdc91811b6cd363f3c5473175af2de92
computercrazy77 <32375789+computercrazy77@users.noreply.github.com>
2019-11-30 16:35:10 -0600

    make tree start empty and add spacing

--- tree.py
+++ tree.py
@@ -7,49 +7,73 @@ class Human:
 # BinaryTree class 
 class BinaryTree: 
    
-    def __init__(self, data): 
-        self.data = data 
+    def __init__(self): 
+        self.data = None 
 
         self.right = None
         self.left = None  
 
 
     def find(self, id):
+        if self.data is None:
+            return None
+
         search = None
+
         if self.data.id == id:
             return self.data
+
         if self.left is not None:
             if self.left.data.id == id:
                 return self.left.data
+
             search = self.left.find(id)
+
         if self.right is not None:
             if self.right.data.id == id:
                 return self.right.data
+                
             search = self.right.find(id)
+
         return search
 
 
     def insert(self, newData):
         if self.data:
+
             if newData.id < self.data.id:
                 if self.left is None:
-                    self.left = BinaryTree(newData)
+
+                    self.left = BinaryTree()
+                    self.left.insert(newData)
+
                 else:
                     self.left.insert(newData)
+            
             elif newData.id > self.data.id:
                 if self.right is None:
-                    self.right = BinaryTree(newData)
+
+                    self.right = BinaryTree()
+                    self.right.insert(newData)
+
                 else:
                     self.right.insert(newData)
+
         else:
-            self.data = data
+            self.data = newData
     
 
     # Print the tree
     def PrintTree(self):
+        if self.data is None:
+            return print("Empty Tree")
+
+        
         if self.left is not None:
             self.left.PrintTree()
+
         print(f"---\nID: {self.data.id}\nNAME: {self.data.name}\nNICKNAME: {self.data.nickname}")
+
         if self.right is not None:
             self.right.PrintTree()
    
@@ -61,15 +85,17 @@ Yana = Human(4, "Yana", "yoyoyana")
 Baba = Human(11, "Baba", "I can't think of a nickname for Baba")
 
 
-people = BinaryTree(Caleb)
+people = BinaryTree()
 
+people.insert(Caleb)
 people.insert(Grandpa)
 people.insert(Yana)
 people.insert(Baba)
+
+people.PrintTree()
 
 result = people.find(int(input("ID to find: ")))
 if result:
     print(f"---\nID: {result.id}\nNAME: {result.name}\nNICKNAME: {result.nickname}")
 else:
     print("Not found.")
-# people.PrintTree()
